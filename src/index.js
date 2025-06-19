ecb811e227c25466eabaf0e7c4893bf733b59a5e
Sunny Park <sp2307@columbia.edu>
2025-06-19 16:46:43 +0100

    fixed exporting a non-existent function

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { LEAFIOmetamodel, _leafstdlib_dataflow_api, _breezyforeststdlib_hierarch
 
 import { reconstructLEAFGraph, analyzeLEAFGraph } from './api/parser/leaf.js';
 import { createGQLClient } from './graphql/client.js'
-import { executeLEAFGraph } from './api/parser/nodelogic/wizardry/leaflisp.js';
 import { doBottle, doUnbottle } from "./api/parser/nodelogic/datautils/bottling.js";
 
 import { runtimeEtaTree } from "./api/parser/eta.js";
@@ -17,4 +16,4 @@ import { initializeLEAFlakeGQLClient } from "./api/leafio/leaflake.js";
 import { _leafgraph } from "./api/parser/nodelogic/abstraction/leafgraph.js";
 import { executeLEAFLogic, executeLEAFLogicInSync } from "./api/parser/leaf.js";
 
-export { executeLEAFLogic, executeLEAFLogicInSync, initializeLEAFlakeGQLClient, init_gRuntimeLEAFlisp, runtimeEtaTree, useGosVolatileStateIO, useGosPersistentStateIO, useMVIframework, useLEAFIOapi, initLEAFIOapi, initializeMasterSubsDirectory, reconstructLEAFGraph, analyzeLEAFGraph, LEAFIOmetamodel, _leafstdlib_dataflow_api, _breezyforeststdlib_hierarchy, createGQLClient, executeLEAFGraph, doBottle, doUnbottle };
+export { executeLEAFLogic, executeLEAFLogicInSync, initializeLEAFlakeGQLClient, init_gRuntimeLEAFlisp, runtimeEtaTree, useGosVolatileStateIO, useGosPersistentStateIO, useMVIframework, useLEAFIOapi, initLEAFIOapi, initializeMasterSubsDirectory, reconstructLEAFGraph, analyzeLEAFGraph, LEAFIOmetamodel, _leafstdlib_dataflow_api, _breezyforeststdlib_hierarchy, createGQLClient, doBottle, doUnbottle };
