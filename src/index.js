9d032095d58f37ae3462ba0fb61ec652afdf73b1
Sunny Park <sp2307@columbia.edu>
2025-06-19 16:54:48 +0100

    more API exposure due to debugging in leaf-node

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { reconstructLEAFGraph, analyzeLEAFGraph } from './api/parser/leaf.js';
 import { createGQLClient } from './graphql/client.js'
 import { doBottle, doUnbottle } from "./api/parser/nodelogic/datautils/bottling.js";
 
-import { runtimeEtaTree } from "./api/parser/eta.js";
+import { runtimeEtaTree, etaReduceDataflowComponent } from "./api/parser/eta.js";
 
 import { init_gRuntimeLEAFlisp } from "./api/parser/nodelogic/wizardry/leaflisp.js";
 import { initializeLEAFlakeGQLClient } from "./api/leafio/leaflake.js";
 import { _leafgraph } from "./api/parser/nodelogic/abstraction/leafgraph.js";
 import { executeLEAFLogic, executeLEAFLogicInSync } from "./api/parser/leaf.js";
 
-export { _leafgraph, executeLEAFLogic, executeLEAFLogicInSync, initializeLEAFlakeGQLClient, init_gRuntimeLEAFlisp, runtimeEtaTree, useGosVolatileStateIO, useGosPersistentStateIO, useMVIframework, useLEAFIOapi, initLEAFIOapi, initializeMasterSubsDirectory, reconstructLEAFGraph, analyzeLEAFGraph, LEAFIOmetamodel, _leafstdlib_dataflow_api, _breezyforeststdlib_hierarchy, createGQLClient, doBottle, doUnbottle };
+export { _leafgraph, executeLEAFLogic, executeLEAFLogicInSync, initializeLEAFlakeGQLClient, init_gRuntimeLEAFlisp, runtimeEtaTree, etaReduceDataflowComponent, useGosVolatileStateIO, useGosPersistentStateIO, useMVIframework, useLEAFIOapi, initLEAFIOapi, initializeMasterSubsDirectory, reconstructLEAFGraph, analyzeLEAFGraph, LEAFIOmetamodel, _leafstdlib_dataflow_api, _breezyforeststdlib_hierarchy, createGQLClient, doBottle, doUnbottle };
